060e56f2864f36b5924825f2a84d8ee299e82578
1dest <ezinnedestiny3@gmail.com>
2022-10-13 17:49:23 -0700

    i created 6size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- the containing func
+  * Return: 0 if no error
+  */
+int main(void)
+{
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(a));
+
+	printf("Size of an int: %ld byte(s)\n", sizeof(b));
+	
+	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+
+	printf("Size of a float: %ld byte(s)\n", sizeof(e));
+	return (0);
+}
