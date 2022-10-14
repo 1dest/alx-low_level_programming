5623c5fedf5f01f0412d37992da4eb52991991be
1dest <ezinnedestiny3@gmail.com>
2022-10-13 18:03:58 -0700

    i created 101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Function begins
+ * Description: 'and that piece of art is useful
+ * Return: Function ends
+  */
+
+int main(void)
+
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
