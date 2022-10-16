0dcd1d86e078116c49582965666b15c5c89b4655
1dest <ezinnedestiny3@gmail.com>
2022-10-16 16:06:34 -0700

    icreated 3 print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - holds my main codes
+ *Return: 0 mean code is (success)
+ */
+
+int main(void)
+{
+	char y;
+	for (y = 'a' ; y <= 'z' ; y++)
+	{
+		putchar(y);
+	}
+	for (y = 'A' ; y <= 'Z' ; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
