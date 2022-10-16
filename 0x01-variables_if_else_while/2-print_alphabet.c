d0fcb1d8d5f5d359afebf8b0252ec183d3174062
1dest <ezinnedestiny3@gmail.com>
2022-10-16 15:36:24 -0700

    i created print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is my entry poin
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+		putchar(s);
+	putchar('\n');
+	return (0);
+}
