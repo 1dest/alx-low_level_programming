01442d45877aae62a2cc2d35a255b2fac545da6b
1dest <ezinnedestiny3@gmail.com>
2022-10-16 17:30:05 -0700

    i creatd 9 print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print single digits
+* Return: will be 0 (succes)
+*/
+
+int main(void)
+{
+	int a;
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
