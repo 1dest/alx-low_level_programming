f616cbacf0b1a3f00d633db9f0f6c445c4e34304
1dest <ezinnedestiny3@gmail.com>
2022-10-19 09:51:59 -0700

    i created 2 print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print 10x
+ *Return - zero value 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int w;
+	int m;
+
+	for (w = 0; w <= 9; w++)
+	{
+		for (m = 'a'; m <= 'z'; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
