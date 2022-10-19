5629f55c2ccdfb2793361e2d537014c93333ea5c
1dest <ezinnedestiny3@gmail.com>
2022-10-19 09:38:49 -0700

    i created 1 alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,14 @@
 #include "main.h"
 /**
- * print_alphabet - this is to print alphabets in lower cases
-* 
+* print_alphabet - this is to print alphabets in lower cases
 * Return: void
 */
 void print_alphabet(void)
+{
+	char s;
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
