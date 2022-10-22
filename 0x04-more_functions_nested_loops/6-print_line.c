b24e073195c090aa0b6f0cfd7d4cd0eee46ec271
1dest <ezinnedestiny3@gmail.com>
2022-10-21 22:49:38 -0700

    i created 6 print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_line - print a new line
+ *@n: parameter
+ */
+
+void print_line(int n)
+{
+
+	int i = 1;
+	while (i <= n)
+	{
+		_putchar(95);
+		i++;
+	}
+	_putchar('\n');
+}
