a55b2830fbb367ccefaec15200ebda7bad2d8e9d
1dest <ezinnedestiny3@gmail.com>
2022-10-24 15:38:52 -0700

    i created 5 rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * rev_string - reverse strings
+  * @s: the var character
+ * Return: the value
+ */
+void rev_string(char *s)
+{
+	int i, e, f;
+	char p, a;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	e = i - 1;
+	f = e / 2;
+	while (f >= 0)
+	{
+		p = s[e - f];
+		a = s[f];
+		s[f] = p;
+		s[e - f] = a;
+		f--;
+	}
+}
