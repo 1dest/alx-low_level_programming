347bdb0d57108dbce745beae9769ec158da6cb3d
1dest <ezinnedestiny3@gmail.com>
2022-11-09 15:02:57 -0800

    i created 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,2 +1,14 @@
-int _putchar(char c);
-int check_num(char *str);
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints the program name
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
