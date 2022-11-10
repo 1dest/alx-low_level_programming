24962c6847ae80eadfb4ab3764a756fca285c009
1dest <ezinnedestiny3@gmail.com>
2022-11-10 15:56:39 -0800

    i created AV

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - print the name of the program
+ * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
   */
 int main(int argc, char *argv[])
 {
